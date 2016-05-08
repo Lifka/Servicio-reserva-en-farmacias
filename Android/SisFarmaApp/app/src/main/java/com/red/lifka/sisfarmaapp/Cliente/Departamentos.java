fe5aa7cd5209b8992dc1092db20bc14c04c3f47a
jcgallardo <jcgallardomorales@gmail.com>
2016-05-08 14:29:37 +0200

    commit

--- Android/SisFarmaApp/app/src/main/java/com/red/lifka/sisfarmaapp/Cliente/Departamentos.java
+++
@@ -1,5 +0,0 @@
-package com.red.lifka.sisfarmaapp.Cliente;
-
-public enum Departamentos {
-    MEDICAMENTOS, HOMEOPATÍA, PERFMERÍA, SIN_CLASIFICAR
-}
--- Android/SisFarmaApp/app/src/main/java/com/red/lifka/sisfarmaapp/Cliente/TipoPago.java
+++
@@ -1,6 +0,0 @@
-package com.red.lifka.sisfarmaapp.Cliente;
-
-
-public enum TipoPago {
-    TARJETA, CONTRARREMBOLSO, PAYPAL
-}
